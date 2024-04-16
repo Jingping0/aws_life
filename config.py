4c6f96b09d88847aae6b2c96eb3644b7f534ab24
Jingping0 <112051172+Jingping0@users.noreply.github.com>
2024-04-16 14:00:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cl0wog2uw9k3.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chiangjingping-employee"
+customregion = "us-east-1"
+
